227778c9f834abaa418062e9a108a43526252c2a
danis <sharipov_danis@bk.ru>
2024-04-05 18:49:11 +0300

    Added double jumo

--- TheRunnerFighter2D/Assets/Scripts/PlayerJump.cs
+++ TheRunnerFighter2D/Assets/Scripts/PlayerJump.cs
@@ -6,6 +6,18 @@ public class PlayerJump : MonoBehaviour
 {
     [SerializeField] private float _jumpForce = 1f;
 
+    [SerializeField] private bool _isGrounded;
+
+    [SerializeField] private Transform _groundCheckObject;
+
+    [SerializeField] private float _checkRadius;
+
+    [SerializeField] private LayerMask _groundLayer;
+
+    [SerializeField] private int _maxJumps = 2;
+
+    private int _jumps = 0;
+
     private Rigidbody2D _rigidBody;
 
     private void Awake()
@@ -14,18 +26,36 @@ public class PlayerJump : MonoBehaviour
     }
     void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump(_jumpForce);
         } 
+
+    }
+    private void FixedUpdate()
+    {
+        _isGrounded = Physics2D.OverlapCircle(_groundCheckObject.position,_checkRadius,_groundLayer);
+
+        if (_isGrounded == true)
+        {
+            _jumps = _maxJumps - 1;
+        }
     }
 
     private void Jump(float jumpForce)
     {
-       _rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-       _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, jumpForce);
-       Debug.Log("Jump!");
-
-
+        if(_jumps > 0)
+        {
+            _rigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            _isGrounded = false;
+            _jumps -= 1;
+            Debug.Log("Jump!");
+        }
+        if(_jumps == 0)
+        {
+            return;
+        }
+      
     }
 }
